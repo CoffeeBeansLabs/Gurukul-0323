96560e8f2b1d87916fd67dfef256964edd2dc037
Tushar Shrivastava <tushar@coffeebeans.io>
2023-03-23 00:12:32 +0530

    [Parking-lot]: commented unneceessary implementations

--- parking_lot/owner.go
+++ parking_lot/owner.go
@@ -1,21 +1,22 @@
 package parking_lot
 
-import "fmt"
-
 type owner struct {
-	isNotified bool
+	//isNotified bool
 }
 
-func NewOwner() *owner {
-	return &owner{
-		isNotified: false,
-	}
-}
+//func NewOwner() *owner {
+//	return &owner{
+//		isNotified: false,
+//	}
+//}
 
 func (o *owner) NotifyParkingFull() {
-	if !o.isNotified {
-		// send notification to owner that parking lot is full
-		fmt.Println("[Owner-notification] : Parking Lot is full !! Please Look for new Lot")
-		o.isNotified = true
-	}
+	//if !o.isNotified {
+	//	// send notification to owner that parking lot is full
+	//	fmt.Println("[Owner-notification] : Parking Lot is full !! Please Look for new Lot")
+	//	o.isNotified = true
+	//}
+}
+
+func (o *owner) NotifyParkingSpaceAvailable() {
 }
